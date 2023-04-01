b7dfca6b45fb9a10bfbf2c1286ba844f66bbc720
8persistent <8persistent@gmail.com>
2023-04-01 22:56:22 +0200

    Input year with validation

--- simple_calendar.py
+++ simple_calendar.py
@@ -9,3 +9,13 @@ format_data = current_data.strftime("%Y-%m-%d | %H:%M")
 
 # Print current date and time
 print(f"Current year: {format_data}")
+
+# ANSI escape codes for coloring input prompts
+RED = "\033[31m"
+RESET = "\033[0m"
+
+# Input year with validation
+year_name = int(input(f"{RED}Enter year: {RESET}"))
+while year_name < 1900 or year_name > 2100:
+    print("Incorrect, please try again!")
+    year_name = int(input(f"{RED}Enter year: {RESET}"))
